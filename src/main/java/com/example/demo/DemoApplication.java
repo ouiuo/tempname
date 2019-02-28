fcec285b217f995ae30e04a614f4cbd5acb31192
ouiuo <valibabaevith@mail.ru>
2019-02-28 17:46:55 +0300

    new comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class DemoApplication {
 //asd
+	// commentline
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
